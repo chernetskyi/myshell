19f4b3b5ceb23adb48ec9eebb81ace59a6964dc9
Volodymyr Chernetskyi <v.v.chernetskyi@gmail.com>
2019-10-20 01:30:27 +0300

    Add readline library

--- src/myshell.cpp
+++ src/myshell.cpp
@@ -1,1 +1,20 @@
+#include <cstdio>
+#include <cctype>
+#include <iostream>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+
 #include "myshell.h"
+
+int main(int argc, char *argv[], char *envp[]) {
+    int erno = 0;
+    char *buff = nullptr;
+    while ((buff = readline("# ")) != nullptr) {
+        if ((strlen(buff) > 0) && !isspace(buff[0]))
+            add_history(buff);
+        std::cout << buff << std::endl;
+        delete buff;
+    }
+    _exit(0);
+}
