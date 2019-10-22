1a68be2f01f8fe059349f99b5e1de2c29fdce45c
nickolanick <bilayev@ucu.edu.ua>
2019-10-22 16:35:21 +0300

    Separating impl from headers

--- inc/myshell.h
+++ inc/myshell.h
@@ -2,11 +2,40 @@
 #define MYSHELL_MYSHELL_H
 
 #include <string>
+#include "builtins.h"
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <vector>
+#include <map>
 
 std::string normalize_input(char *input);
 
-void execute(const std::string &input, char *envp[], int &errn);
+class MyShell {
+private:
+    int erno = 0;
+    char *buff = nullptr;
+    const char *prompt = "# ";
+    char **envp;
+
+    std::map<std::string, builtin> builtins_map;
+
+    void initialize_builtins();
+
+    builtin builtins(const std::string &command);
+
+public:
+    explicit MyShell(char *envp[]) : envp(envp) {
+        initialize_builtins();
+    }
+
+
+    void start();
+
+    void execute(const std::string &input);
+};
 
 constexpr const char command_not_found_error[] = "myshell: commnad not found: ";
+
 
 #endif //MYSHELL_MYSHELL_H
--- src/myshell.cpp
+++ src/myshell.cpp
@@ -16,11 +16,6 @@
 #include "myshell.h"
 #include <iostream>
 #include <map>
-#include <string>
-
-#include <boost/program_options.hpp>
-
-#include "builtins.h"
 
 
 std::string normalize_input(char *input) {
@@ -32,51 +27,41 @@ std::string normalize_input(char *input) {
 }
 
 
-class MyShell {
-    int erno = 0;
-    char *buff = nullptr;
-    const char *prompt = "# ";
-    char **envp;
-
-    std::map<std::string, builtin> builtins_map;
-public:
-    explicit MyShell(char *envp[]) : envp(envp) {
-
-        builtins_map["mexit"] = &mexit;
-        builtins_map["merrno"] = [this](int argc, char **argv, char **envp) { return merrno(argc, argv, envp, erno); };
+void MyShell::execute(const std::string &input) {
+    std::vector<std::string> splits;
+    boost::split(splits, input, boost::is_space());
+    if (builtins(splits[0]) != nullptr) {
+        builtin func = builtins(splits[0]);
+        char **margv = new char *[splits.size()];
+        for (size_t i = 0; i < splits.size(); ++i)
+            margv[i] = const_cast<char *>(splits[i].c_str());
+        erno = func(splits.size(), margv, envp);
+        delete[] margv;
+    } else {
+        std::cerr << command_not_found_error << splits[0] << std::endl;
+        erno = 1;
     }
+}
 
-    builtin builtins(const std::string &command) {
-        return builtins_map.find(command) != builtins_map.end() ? builtins_map[command] : nullptr;
-    }
-
-
-    void start() {
-        while ((buff = readline(prompt)) != nullptr) {
-            if ((strlen(buff) > 0) && !isspace(buff[0]))
-                add_history(buff);
-            std::string user_input = normalize_input(buff);
-            free(buff);
-            execute(user_input);
-        }
-    };
-
-    void execute(const std::string &input) {
-        std::vector<std::string> splits;
-        boost::split(splits, input, boost::is_space());
-        if (builtins(splits[0]) != nullptr) {
-            builtin func = builtins(splits[0]);
-            char **margv = new char *[splits.size()];
-            for (size_t i = 0; i < splits.size(); ++i)
-                margv[i] = const_cast<char *>(splits[i].c_str());
-            erno = func(splits.size(), margv, envp);
-            delete[] margv;
-        } else {
-            std::cerr << command_not_found_error << splits[0] << std::endl;
-            erno = 1;
-        }
+void MyShell::start() {
+    while ((buff = readline(prompt)) != nullptr) {
+        if ((strlen(buff) > 0) && !isspace(buff[0]))
+            add_history(buff);
+        std::string user_input = normalize_input(buff);
+        free(buff);
+        execute(user_input);
     }
 };
+
+
+builtin MyShell::builtins(const std::string &command) {
+    return builtins_map.find(command) != builtins_map.end() ? builtins_map[command] : nullptr;
+}
+
+void MyShell::initialize_builtins() {
+    builtins_map["mexit"] = &mexit;
+    builtins_map["merrno"] = [this](int argc, char **argv, char **envp) { return merrno(argc, argv, envp, erno); };
+}
 
 
 int main(int argc, char *argv[], char *envp[]) {
