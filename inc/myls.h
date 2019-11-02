9334fd0bceae1fc3df044667389a239f1cb0ce1b
Andrii <andykoval@ucu.edu.ua>
2019-11-02 19:16:07 +0200

    Sorting with different options and stats

--- inc/myls.h
+++ inc/myls.h
@@ -17,7 +17,7 @@ typedef struct {
 
 void parse_sorting_option(options_struct &options, std::vector<std::string> value);
 
-void error(int ec, std::string msg);
+void error(int ec, const std::string& msg);
 
 constexpr char invalid_option_error[] = "myls: invalid option -- ";
 constexpr char try_help_message[] = "Try 'myls --help' for more information.";
--- src/myls.cpp
+++ src/myls.cpp
@@ -5,10 +5,64 @@
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
+#include <sys/stat.h>
+
 #include "myls.h"
 
 namespace po = boost::program_options;
 using namespace boost::filesystem;
+
+bool is_special(const mode_t &st_mode) {
+    return !S_ISDIR(st_mode) || !S_ISREG(st_mode);
+}
+
+bool is_dir(const mode_t &st_mode) {
+    return S_ISDIR(st_mode);
+}
+
+bool file_comparator(const directory_entry &file1, const directory_entry &file2, options_struct &opts,
+        std::map<directory_entry, struct stat> stats) {
+
+    struct stat x = stats[file1], y = stats[file2];
+
+    if (opts.directories_first && is_dir(x.st_mode)) {
+        if (!is_dir(y.st_mode)) return true;
+    } else if (opts.directories_first && is_dir(y.st_mode)) return false;
+
+    if (opts.separate_special_files && is_special(x.st_mode)) {
+        if (!is_special(y.st_mode)) return true;
+    } else if (opts.separate_special_files && is_special(y.st_mode)) return false;
+
+
+    bool return_flag = false;
+    bool equal = false;
+
+    switch (opts.sort_method) {
+        case 'U':
+            return_flag = false;
+            break;
+        case 'S':
+            if (x.st_size == y.st_size) equal = true;
+            return_flag = x.st_size > y.st_size;
+            break;
+        case 't':
+            if (x.st_mtimespec.tv_sec == y.st_mtimespec.tv_sec) equal = true;
+            return_flag = x.st_mtimespec.tv_sec > y.st_mtimespec.tv_sec;
+            break;
+        case 'X':
+            if (file1.path().extension() == file2.path().extension()) equal = true;
+            return_flag = file1.path().extension() < file2.path().extension();
+            break;
+        case 'N':
+            return_flag = file1.path().string() < file2.path().string();
+            break;
+    }
+
+    if (equal) return_flag = file1.path().string() < file2.path().string();
+    if (opts.reverse) return_flag = !return_flag;
+
+    return return_flag;
+}
 
 
 std::string trailing_slesh(std::string file) {
@@ -22,11 +76,22 @@ void print_file(directory_entry &file) {
 }
 
 void list_dirs(options_struct &opts) {
+    std::map<directory_entry, struct stat> stats;
+
     for (auto &directory : opts.files) {
         path p(directory);
         std::vector<directory_entry> v;
         if (is_directory(p)) {
             copy(directory_iterator(p), directory_iterator(), back_inserter(v));
+            for (auto &file:v) {
+                struct stat tmp{};
+                stat(const_cast<char *>(file.path().string().c_str()), &tmp);
+                stats[file] = tmp;
+            }
+            std::sort(v.begin(), v.end(),
+                    [&opts, &stats] (const directory_entry& x, const directory_entry& y) {
+                return file_comparator(x, y, opts, stats);
+            });
             std::cout << p.stem() << ":" << std::endl;
             for (auto &file : v) {
                 print_file(file);
@@ -145,7 +210,7 @@ void parse_sorting_option(options_struct &options, std::vector<std::string> valu
     }
 }
 
-void error(int ec, std::string msg) {
+void error(int ec, const std::string& msg) {
     std::cerr << msg << std::endl;
     exit(ec);
 }
