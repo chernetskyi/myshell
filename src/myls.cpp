ecbbd7d031182b8d598290f5270a3f8994e040ca
nickolanick <bilayev@ucu.edu.ua>
2019-11-02 17:19:21 +0200

    Parse F and l flags

--- src/myls.cpp
+++ src/myls.cpp
@@ -5,6 +5,8 @@
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <boost/range/adaptor/reversed.hpp>
+#include <sys/stat.h>
+
 
 #include "myls.h"
 
@@ -12,14 +14,56 @@ namespace po = boost::program_options;
 using namespace boost::filesystem;
 
 
-std::string trailing_slesh(std::string file) {
+std::string trailing_slesh(std::string &file) {
     return is_directory(file) ? "/" + file : file;
 }
 
+
+std::string f_flag(std::string filename, struct stat &stat_buff) {
+
+    if (stat_buff.st_mode & S_IXUSR)
+        filename = "*" + filename;
+    else {
+        switch (stat_buff.st_mode & S_IFMT) {
+            case S_IFDIR:
+                filename = "/" + filename;
+                break;
+            case S_IFLNK:
+                filename = "@" + filename;
+                break;
+            case S_IFSOCK:
+                filename = "=" + filename;
+                break;
+            default:
+                filename = "?" + filename;
+
+        }
+    }
+    return filename + "\t";
+}
+
+std::string l_flag(std::string &filename, struct stat &stat_buff) {
+    return trailing_slesh(filename) + "\t"+ std::to_string(stat_buff.st_size)+ "\n";
+}
+
 std::string format_file(directory_entry &file, options_struct &flags) {
+    FILE *fp;
+    struct stat stat_buff;
+    if ((fp = fopen(file.path().c_str(), "rb")) == NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    fstat(fileno(fp), &stat_buff);
+
     std::string filename = file.path().filename().string();
-    std::cout<<"filename extension "<<extension(file)<<" xt"<<std::endl;
-    return trailing_slesh(filename) + "\t";
+
+
+    if (flags.indicate)
+        filename = f_flag(filename, stat_buff);
+    else if (flags.long_listing)
+        filename = l_flag(filename, stat_buff);
+
+    return filename ;
 
 }
 
@@ -43,10 +87,10 @@ int main(int argc, char *argv[], char *envp[]) {
     basic_options.add_options()
 
             ("help,h", "print help message")
-            (",l", "use a long listing format")
-            (",r", "reverse order while sorting")
-            (",F", "append indicator to entries")
-            (",R", "list subdirectories recursively")
+            ("l", "use a long listing format")
+            ("r", "reverse order while sorting")
+            ("F", "append indicator to entries")
+            ("R", "list subdirectories recursively")
             ("sort", po::value<std::vector<std::string>>()->default_value(std::vector<std::string>{"N"}, "N"),
              "sort by parameter instead of name");
     po::options_description hidden_options("Hidden options");
@@ -73,6 +117,7 @@ int main(int argc, char *argv[], char *envp[]) {
     opts.reverse = vm.count("r") != 0;
     opts.indicate = vm.count("F") != 0;
     opts.recursive = vm.count("R") != 0;
+
 
     list_dirs(opts);
 
